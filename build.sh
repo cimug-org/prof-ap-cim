d5f9941c35cc4167cb1f751df9b4760497446536
Bart Kleijngeld <bartkl@protonmail.com>
2026-05-22 17:29:45 +0200

    WIP

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 jinja2 src/prof_ap_cim.adoc.jinja2 src/prof_ap_cim.linkml.yml \
     | tee docs/index.adoc \
-    | asciidoctor -o docs/index.html -
+    | asciidoctor -o docs/index.html -r rouge -
 
 cp \
     src/prof_ap_cim.linkml.yml \
     src/prof_ap_cim.shacl.ttl \
+    src/prof_ap_cim.context.jsonld \
     src/prof_ap_cim.uml.svg \
 docs/
