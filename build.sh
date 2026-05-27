94f44d111ea8031b56215c892da389ea91c20432
Bart Kleijngeld <bartkl@protonmail.com>
2026-05-27 09:39:48 +0200

    2.1: Antora structure and better separation of jinja/adoc

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/bin/env bash
 
+set -e
+
 rm -rf docs-adoc
 mkdir -p docs-adoc
 
 for src_dir in src/*; do
-    linkml=$src_dir/schema/prof_ap_cim.linkml.yml
     version=$(basename $src_dir)
     root_module=docs-adoc/$version/modules/ROOT
+
+    if [ $version == "2.1" ]; then
+        cp -r $src_dir docs-adoc/
+        root_module=docs-adoc/$version/modules/ROOT
+        linkml=$src_dir/modules/ROOT/attachments/schema/prof_ap_cim.linkml.yml
+        jinja2 -o $root_module/partials/preamble.adoc $src_dir/modules/ROOT/partials/preamble.adoc $linkml
+        jinja2 -o $root_module/partials/metadata.adoc $src_dir/modules/ROOT/partials/metadata.adoc $linkml
+        jinja2 -o $root_module/partials/header.adoc $src_dir/modules/ROOT/partials/header.adoc $linkml
+        jinja2 -o $root_module/partials/schema-diagram.d2 $src_dir/modules/ROOT/partials/schema-diagram.d2 $linkml
+        jinja2 -o $root_module/partials/schema-classes.adoc $src_dir/modules/ROOT/partials/schema-classes.adoc $linkml
+        jinja2 -o $root_module/partials/schema-namespaces.adoc $src_dir/modules/ROOT/partials/schema-namespaces.adoc $linkml
+        jinja2 -o $root_module/pages/index.adoc $src_dir/modules/ROOT/pages/index.adoc $linkml
+        continue
+    fi
+
+    linkml=$src_dir/schema/prof_ap_cim.linkml.yml
 
     mkdir -p $root_module/{attachments,images,pages}
     mkdir $root_module/attachments/examples
